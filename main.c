acd03f87be83779196e87931bc96702885ac3a98
Md1o1 <kelvinmendes8@gmail.com>
2024-11-27 20:17:15 -0300

    setup inicial para o projeto

---
+++ main.c
@@ -0,0 +1,26 @@
+/*********************************************************************
+ * objetivo: Sistema para gerenciar a companhia aérea Vôo Tranquilo.
+ * autores: Hugo Tavares Dutra, Kelvin Gustavo Fernandes Mendes.
+ * versão: 1.0
+ *********************************************************************/
+
+//Inclusão de bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <locale.h>
+#include "modulos.h"
+
+// definir horas, dias, estruturas de dados, limites de registros
+
+// definir arquivos onde os dados trabalhados serão armazenados
+
+// declaração de procedimentos e funções
+void teste();
+
+//Programa principal
+int main(){
+    teste();
+    return 0;
+}
---
+++ modulos.h
@@ -0,0 +1,4 @@
+// Funções e procedimentos para o programa main.c, do sistema de gerenciamento da companhia aérea Vôo Tranquilo.
+void teste(){
+   printf("Sistema em desenvolvimento! :) \n");
+}
